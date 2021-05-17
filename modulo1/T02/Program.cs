0c13d54fe625c73dd7b04877882c7b82a9b0e3cc
giorocor <gio-giovanny@hotmail.com>
2021-05-17 21:20:46 +0200

    iniciando en .net "hola mundo" T02

---
+++ modulo1/T02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace T02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+            Console.ReadKey();
+
+        }
+    }
+}
